972101eaaa4ffcd2dbc490baeed943d39218cbea
Thigoo <thiagomvk08@gmail.com>
2026-08-05 18:00:29 -0300

    conversão de pdf para md

---
+++ AULA_02/converter.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from docling.document_converter import DocumentConverter
+import os
+
+os.environ["TORCHDYNAMO_DISABLE"] = "1"
+
+pdf_dir = Path("AULA_02/files")
+output_dir = Path("AULA_02/output")
+
+output_dir.mkdir(parents=True, exist_ok=True)
+
+converter = DocumentConverter()
+
+for file in pdf_dir.glob("*.pdf"):
+    print(f"Convertendo {file.name}")
+
+    result = converter.convert(str(file))
+
+    output_file = output_dir / f"{file.stem}.md"
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(result.document.export_to_markdown())
+
+    print(f"Arquivo salvo em {output_file.resolve()}")
+
+print("Conversão concluída!")
